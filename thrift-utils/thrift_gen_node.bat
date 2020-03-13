3628cf91ec0753f816d1865f9f956f6fe64537f5
0xAAE <avramenko.a@gmail.com>
2020-03-13 10:13:28 +0300

    Add thrift utils

---
+++ thrift-utils/thrift_gen_node.bat
@@ -0,0 +1,3 @@
+thrift.exe -r --gen csharp -out ../NodeAPI/Api ../third-party/thrift-interface-definitions/api.thrift
+thrift.exe -r --gen csharp -out ../NodeAPI/Api ../third-party/thrift-interface-definitions/apidiag.thrift
+thrift.exe -r --gen csharp -out ../NodeAPI/Api ../third-party/thrift-interface-definitions/apiexec.thrift
