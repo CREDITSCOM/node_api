d56617aa9797587f4bf3624a3a9869a1f3677410
0xAAE <avramenko.a@gmail.com>
2020-03-16 16:59:20 +0300

    fix generate bat file

--- thrift-utils/thrift_gen_node.bat
+++ thrift-utils/thrift_gen_node.bat
@@ -1,3 +1,4 @@
-thrift.exe -r --gen csharp -out ../NodeAPI/Api ../third-party/thrift-interface-definitions/api.thrift
-thrift.exe -r --gen csharp -out ../NodeAPI/Api ../third-party/thrift-interface-definitions/apidiag.thrift
-thrift.exe -r --gen csharp -out ../NodeAPI/Api ../third-party/thrift-interface-definitions/apiexec.thrift
+mkdir ..\NodeAPIClient\Api
+thrift.exe -r --gen csharp -out ../NodeAPIClient/Api ../third-party/thrift-interface-definitions/api.thrift
+thrift.exe -r --gen csharp -out ../NodeAPIClient/Api ../third-party/thrift-interface-definitions/apidiag.thrift
+thrift.exe -r --gen csharp -out ../NodeAPIClient/Api ../third-party/thrift-interface-definitions/apiexec.thrift
